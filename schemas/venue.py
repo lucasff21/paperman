cd01ee8bd418bfd9e77a91ba716c8318025a3341
Marcos Vinícius Queiroz de Sant'Ana Filho <kieroth29@gmail.com>
2025-07-06 22:49:39 -0300

    fix: Normalizing year and venue if structured in list

--- schemas/venue.py
+++ schemas/venue.py
@@ -1,11 +1,21 @@
-from typing import Optional
-from pydantic import BaseModel
+from typing import List, Optional, Union
+from pydantic import BaseModel, field_validator
 
 
 class VenueInfo(BaseModel):
-    venue: str
+    venue: Optional[str] = None
     type: str
     url: str
+
+    @field_validator("venue")
+    def validate_venue(cls, value: Union[str, List[str]]) -> str:
+        if not value:
+            return None
+        
+        if isinstance(value, list):
+            return ', '.join(map(str, value))
+        
+        return value
     
 
 class Venue(BaseModel):
